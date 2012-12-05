8eeb0eed6ac159e9f622d63eb65cddc457ab48ab
Aksel <aksel.ethem@gmail.com>
2012-12-05 18:08:20 +0100

    first commit

---
+++ codesharing/cab/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+from pygments import lexers, formatters, highlight
+from tagging.fields import TagField
+from django.contrib.auth.models import User
+from markdown import markdown
+import datetime
+
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    language_code = models.CharField(max_length=50)
+    mime_type = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['name']
+
+    def __unicode__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('cab_language_detail', kwargs={'slug': self.slug})
+
+    def get_lexer(self):
+        return lexers.get_lexer_by_name(self.language_code)
+
+
+class Snippet(models.Model):
+    title = models.CharField(max_length=255)
+    language = models.ForeignKey(Language)
+    author = models.ForeignKey(User)
+    description = models.TextField()
+    description_html = models.TextField(editable=False)
+    code = models.TextField()
+    highlighted_code = models.TextField(editable=False)
+    tags = TagField()
+    pub_date = models.DateTimeField(editable=False)
+    update_date = models.DateTimeField(editable=False)
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __unicode__(self):
+        return self.title
+
+    def highlight(self):
+        ''' Args: code to highlight, lexer to use, formatter '''
+        return highlight(self.code, self.language.get_lexer(),
+                                    formatters.HtmlFormatter(lineos=True))
+
+    def save(self, force_insert=False, force_update=False):
+        if not self.id:
+            self.pub_date = datetime.datetime.now()
+        self.updated_date = datetime.datetime.now()
+
+        self.description_html = markdown(self.description)
+        self.highlighted_code = self.higlight()
+        super(Snippet, self).save(force_insert, force_update)
+
+    def get_absolute_url(self):
+        return reverse('cab_snippet_detail', kwargs={'object_id':self.id})
+
